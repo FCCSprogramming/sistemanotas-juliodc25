7e08089b274d67bfcc24370bdad6453800d3cfd1
juliodc25 <julio.diaz.c@uni.pe>
2025-11-18 00:05:55 -0500

    programa terminado,pero a la hora de ingresar una opcion y hacer el procedimiento de la opcion elegida el programa entra en bucle del menu, no he podido encontrar el error en el codigo que causa eso

--- sistemaUni.cpp
+++ sistemaUni.cpp
@@ -51,8 +51,6 @@ struct Estudiante {
 
 //gestion de estudiantes
 void registrarEstudiante(Estudiante estudiantes[],int &cantidad);
-void mostrarEstudiantes(const Estudiante estudiantes[],int cantidad);
-Estudiante* buscarEstudiante(Estudiante estudiantes[],int cantidad,string codigo);
 //gestion de cursos por estudiante
 void matricularCurso(Estudiante &estudiante);
 void ingresarNotas(Estudiante &estudiante);
@@ -72,10 +70,11 @@ int main(){
     Estudiante* ptrEstudiantes;
     ptrEstudiantes= estudiantes;
     int cantidadEstudiantes = 0;
+    int opcion;
     for(int i = 0; i < 100; i++){
         estudiantes[i].cantidadCursos = 0;
     }
-    while(true){
+    do{
         cout << "Sistema de Gestion Academica - UNI" << endl;
         cout << "1. Registrar Estudiante" << endl;
         cout << "2. Matricular curso a estudiante" << endl;
@@ -88,7 +87,7 @@ int main(){
         cout << "9. Salir" << endl;
         cout << "Seleccione una opcion: ";
         
-        int opcion;
+        
         cin >> opcion;
         
         switch (opcion)
@@ -103,6 +102,7 @@ int main(){
             cout<<"=== MATRICULAR CURSO ==="<<endl;
             cout<<"Estudiante: ";
             getline(cin, codigoEstudiante);
+            cin.ignore();
             getline(cin, nombreEstudiante);
             for(int i = 0; i < cantidadEstudiantes; i++){
                 if(ptrEstudiantes->codigo == codigoEstudiante && ptrEstudiantes->nombre == nombreEstudiante){
@@ -118,6 +118,7 @@ int main(){
             string nombreEstudiante;
             cout<<"Nombre Estudiante: ";
             getline(cin, codigoEstudiante);
+            cin.ignore();
             getline(cin, nombreEstudiante);
             for(int i = 0; i < cantidadEstudiantes; i++){
                 if(ptrEstudiantes->codigo == codigoEstudiante && ptrEstudiantes->nombre == nombreEstudiante){
@@ -133,6 +134,7 @@ int main(){
             string nombreEstudiante;
             cout<<"Nombre Estudiante: ";
             getline(cin, codigoEstudiante);
+            cin.ignore();
             getline(cin, nombreEstudiante);
             for(int i = 0; i < cantidadEstudiantes; i++){
                 if(ptrEstudiantes->codigo == codigoEstudiante && ptrEstudiantes->nombre == nombreEstudiante){
@@ -149,6 +151,7 @@ int main(){
             string nombreEstudiante;
             cout<<"Nombre Estudiante: ";
             getline(cin, codigoEstudiante);
+            cin.ignore();
             getline(cin, nombreEstudiante);
             for(int i = 0; i < cantidadEstudiantes; i++){
                 if(ptrEstudiantes->codigo == codigoEstudiante && ptrEstudiantes->nombre == nombreEstudiante){
@@ -161,23 +164,28 @@ int main(){
             
             break;
         case 6:
-            
+            mostrarMejorPromedio(estudiantes, cantidadEstudiantes);
             break;
         case 7:
-            
+            cout<<"Codigo del curso: ";
+            {
+                string codigoCurso;
+                getline(cin, codigoCurso);
+                cin.ignore();
+                estudiantesAprobadosCurso(estudiantes, cantidadEstudiantes, codigoCurso);
+            }
             break;
         case 8:
-            
+            reporteGeneral(estudiantes, cantidadEstudiantes);
+            break;
+        case 9:
+            cout<<"Saliendo del sistema..."<<endl;
             break;
         default:
             break;
         }
-        
-        if(opcion == 9){
-            break; // Salir del sistema
-        }
-    
-    }
+           
+    }while(opcion != 9);
 
     return 0;
 }
@@ -186,13 +194,17 @@ void registrarEstudiante(Estudiante estudiantes[],int &cantidad){
     cout<<"=== REGISTRAR ESTUDIANTE ==="<<endl;
     cout<<"Codigo: ";
     getline(cin, estudiantes[cantidad].codigo);
+    cin.ignore();
     cout<<"Nombre: ";
     getline(cin, estudiantes[cantidad].nombre);
+    cin.ignore();
     cout<<"Carrera: ";
     getline(cin, estudiantes[cantidad].carrera);
+    cin.ignore();
     cout<<"Ciclo: ";
     cin>>estudiantes[cantidad].ciclo;
 }
+
 void matricularCurso(Estudiante &estudiante){
     if(estudiante.cantidadCursos >= 6){
         cout<<"El estudiante ya tiene el maximo de cursos matriculados."<<endl;
@@ -201,8 +213,10 @@ void matricularCurso(Estudiante &estudiante){
     Curso &curso = estudiante.cursos[estudiante.cantidadCursos];
     cout<<"Codigo curso: ";
     getline(cin, curso.codigo);
+    cin.ignore();
     cout<<"Nombre: ";
     getline(cin, curso.nombre);
+    cin.ignore();
     cout<<"Creditos: ";
     cin>>curso.creditos;
     cout<<"Profesor: ";
@@ -215,6 +229,7 @@ void ingresarNotas(Estudiante &estudiante) {
     string nombreCurso;
     cout<<"Curso: ";
     getline(cin, codigoCurso);
+    cin.ignore();
     getline(cin, nombreCurso);
     for(int i = 0; i < estudiante.cantidadCursos; i++){
         if(estudiante.cursos[i].codigo == codigoCurso && estudiante.cursos[i].nombre == nombreCurso){
@@ -247,4 +262,56 @@ void generarBoleta(const Estudiante &estudiante){
        cout<<setw(6)<<std::left<<estudiante.cursos[i].codigo<<" "<<setw(30)<<std::left<<estudiante.cursos[i].nombre<<" "<<setw(8)<<std::left<<estudiante.cursos[i].creditos<<" "<<setw(2)<<std::left<<estudiante.cursos[i].notas.EP<<" "<<setw(2)<<std::left<<estudiante.cursos[i].notas.EF<<" "<<setw(2)<<std::left<<estudiante.cursos[i].notas.PP<<" "<<setw(8)<<std::left<<notas.calcularPromedio()<<" "<<setw(10)<<std::left<<notas.obtenerEstado();
     }
         
+}
+int calcularTotalCreditos(const Estudiante &estudiante){
+    int totalCreditos = 0;
+    for(int i = 0; i < estudiante.cantidadCursos; i++){
+        totalCreditos += estudiante.cursos[i].creditos;
+    }
+    return totalCreditos;
+}
+float calcularPromedioPonderado(const Estudiante &estudiante){
+    float sumaPromedios = 0.0;
+    Notas notas= estudiante.cursos->notas;
+    for(int i = 0; i < estudiante.cantidadCursos; i++){
+        sumaPromedios += notas.calcularPromedio();
+        notas = estudiante.cursos[i+1].notas;
+    }
+    return sumaPromedios / estudiante.cantidadCursos;
+}
+void mostrarMejorPromedio(const Estudiante estudiantes[],int cantidad){
+   float mejorPromedio = 0.0;
+   string nombreMejorEstudiante;
+   for(int i = 0; i < cantidad; i++){
+       float promedioEstudiante = calcularPromedioPonderado(estudiantes[i]);
+       if(promedioEstudiante > mejorPromedio){
+           mejorPromedio = promedioEstudiante;
+           nombreMejorEstudiante = estudiantes[i].nombre;
+       }
+   }
+   cout<<"Alumno: "<<nombreMejorEstudiante<<" con promedio "<<mejorPromedio<<endl;
+}
+void estudiantesAprobadosCurso(const Estudiante estudiantes[],int cantidad,string codigoCurso){
+    cout<<"=== ESTUDIANTES APROBADOS EN CURSO "<<codigoCurso<<" ==="<<endl;
+    for(int i = 0; i < cantidad; i++){
+        for(int j = 0; j < estudiantes[i].cantidadCursos; j++){
+            if(estudiantes[i].cursos[j].codigo == codigoCurso){
+                Notas notas = estudiantes[i].cursos[j].notas;
+                if(notas.obtenerEstado() == "Aprobado"){
+                    cout<<"Estudiante: "<<estudiantes[i].nombre<<" ("<<estudiantes[i].codigo<<") - Promedio: "<<notas.calcularPromedio()<<endl;
+                }
+            }
+        }
+    }
+
+}
+void reporteGeneral(const Estudiante estudiantes[],int cantidad){
+    cout<<"=== REPORTE GENERAL DE ESTUDIANTES ==="<<endl;
+    for(int i = 0; i < cantidad; i++){
+        cout<<"Estudiante: "<<estudiantes[i].nombre<<" ("<<estudiantes[i].codigo<<")"<<endl;
+        cout<<"Carrera: "<<estudiantes[i].carrera<<" - Ciclo: "<<estudiantes[i].ciclo<<endl;
+        cout<<"Cursos Matriculados: "<<estudiantes[i].cantidadCursos<<endl;
+        cout<<"Total Creditos: "<<calcularTotalCreditos(estudiantes[i])<<endl;
+        cout<<"Promedio Ponderado: "<<calcularPromedioPonderado(estudiantes[i])<<endl;
+    }
 }
